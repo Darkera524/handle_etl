c6b34581a6cc85e003293dd500c648ced8b8eea7
王一琦 <wangyiqi@gridsum.com>
2017-11-20 17:09:04 +0800

    transfer etl to xml every 5min

--- etl/etl.go
+++ etl/etl.go
@@ -6,15 +6,35 @@ import (
 	"bytes"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"fmt"
 )
 
-func Exec_tracerpt(){
-	yesterday := get_yesterday()
-	filename := "E:\\percatch\\" + yesterday + "\\DataCollector01.etl"
-	idcshare := "\\\\idcshare.op.internal.gridsumdissector.com\\idcshare\\wangyiqi\\xml\\" + yesterday + ".xml"
-	c := exec.Command("tracerpt", filename , "-o", idcshare, "-lr", "-of", "XML")
-	c.Stdout = os.Stdout
-	c.Run()
+func Exec_tracerpt() {
+	var interval int64 = 300
+	var ticker= time.NewTicker(time.Duration(interval) * time.Second)
+
+	for {
+		exec_inter()
+		<-ticker.C
+	}
+}
+
+func exec_inter(){
+	dirname := dir_name()
+	if (dirname == ""){
+
+	} else{
+
+		filename := dirname+"\\DataCollector01.etl"
+		path_list := strings.Split(dirname,"\\")
+		single_dir_name := path_list[len(path_list)-1]
+		//"E:\\percatch\\" + yesterday + "\\DataCollector01.etl"
+		idcshare := "\\\\idcshare.op.internal.gridsumdissector.com\\idcshare\\wangyiqi\\xml\\" + single_dir_name + ".xml"
+		c := exec.Command("tracerpt", filename, "-o", idcshare, "-lr", "-of", "XML")
+		c.Stdout = os.Stdout
+		c.Run()
+	}
 
 }
 
@@ -28,4 +48,22 @@ func get_yesterday() string {
 	}
 	yesterday := buffer.String()
 	return yesterday
+}
+
+func dir_name() string {
+	old_ins := ""
+	new_ins := ""
+	path := "C:\\PerfLogs\\Admin\\domain-collector\\"
+	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		if ( f == nil ) {return err}
+		if !f.IsDir() {return nil}
+		old_ins = new_ins
+		new_ins = path
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
+
+	return old_ins
 }
